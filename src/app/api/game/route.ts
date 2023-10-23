c24ecf67009878ba812f825f3922bf5027fa07ac
seraph <106294171+seraph-yang@users.noreply.github.com>
2023-10-22 21:09:37 -0700

    Update route.ts

--- src/app/api/game/route.ts
+++ src/app/api/game/route.ts
@@ -5,7 +5,7 @@ import { NextResponse } from "next/server";
 import { z } from "zod";
 import axios from "axios";
 
-export const maxDuration = 5;
+export const maxDuration = 300;
 
 export async function POST(req: Request, res: Response) {
   try {
